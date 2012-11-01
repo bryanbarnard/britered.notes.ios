610d7091494aed4b6d799d61e28b79dcf1943743
Bryan Barnard <bmbarnard@gmail.com>
2012-10-31 22:36:35 -0500

    baseline

---
+++ bbarnard_csc471_FinalProject/bbarnardAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  bbarnardAppDelegate.h
+//  bbarnard_csc471_FinalProject
+//
+//  Created by Bryan Barnard on 10/31/12.
+//  Copyright (c) 2012 Bryan Barnard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface bbarnardAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ bbarnard_csc471_FinalProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  bbarnard_csc471_FinalProject
+//
+//  Created by Bryan Barnard on 10/31/12.
+//  Copyright (c) 2012 Bryan Barnard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "bbarnardAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([bbarnardAppDelegate class]));
+    }
+}
