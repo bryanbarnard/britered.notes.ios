cd57b6d75079316fccd474ab5ff1e260d6a25538
Bryan Barnard <bmbarnard@gmail.com>
2012-10-31 22:52:05 -0500

    creating views

---
+++ bbarnard_csc471_FinalProject/bbarnard_LoginPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  bbarnard_LoginPageViewController.h
+//  bbarnard_csc471_FinalProject
+//
+//  Created by Bryan Barnard on 10/31/12.
+//  Copyright (c) 2012 Bryan Barnard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface bbarnard_LoginPageViewController : UIViewController
+
+@end
---
+++ bbarnard_csc471_FinalProject/bbarnard_LoginPageViewController.m
@@ -0,0 +1,38 @@
+//
+//  bbarnard_LoginPageViewController.m
+//  bbarnard_csc471_FinalProject
+//
+//  Created by Bryan Barnard on 10/31/12.
+//  Copyright (c) 2012 Bryan Barnard. All rights reserved.
+//
+
+#import "bbarnard_LoginPageViewController.h"
+
+@interface bbarnard_LoginPageViewController ()
+
+@end
+
+@implementation bbarnard_LoginPageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
