d3e9dabf94137e89670b9263e71310578399d149
Bryan Barnard <bmbarnard@gmail.com>
2012-11-03 19:46:58 -0500

    updates

---
+++ bbarnard_csc471_FinalProject/bbarnard_AboutPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  bbarnard_AboutPageViewController.h
+//  bbarnard_csc471_FinalProject
+//
+//  Created by Bryan Barnard on 11/3/12.
+//  Copyright (c) 2012 Bryan Barnard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface bbarnard_AboutPageViewController : UIViewController
+
+@end
